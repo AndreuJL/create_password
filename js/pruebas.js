3718d5ad2f1fcca3889b39685fda089e7a20c9eb
Andreu Julià lliteras <andreujulia@estudiante.edib.es>
2024-01-31 13:14:10 +0100

    Haciendo comprobaciones

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,25 +1,30 @@
-let contrasenya = ["a", "u", "a", "P", "i", "E"];
+let contrasenya = "a/hPa¿?E";
+let comprobante = true;
 
-for (let i = 0; i < contrasenya.length; i++) {
-  let comprobante = true;
-  let caracter_a_comprobar = contrasenya[i];
+do {
+
+  // Todo el código
+
   
-  // Que no se repitan carácteres
-  for (let j = 0; j < contrasenya.length; j++) {
-    let contenido_indice = contrasenya[j];
-    if (caracter_a_comprobar === contenido_indice) {
-      
-      if (i !== j && caracter_a_comprobar === contenido_indice) {
-        console.log(caracter_a_comprobar + " está repetido");
-
-        let comprobante = false;  // COn un do while en el otro sitio que solo deja pasar si es true, sinó vuelve atrás.
+  for (let i = 0; i < contrasenya.length; i++) {   
+    let caracter_a_comprobar = contrasenya[i];
+    
+    // Que no se repitan carácteres
+    for (let j = 0; j < contrasenya.length; j++) {
+      let contenido_indice = contrasenya[j];
+      if (caracter_a_comprobar === contenido_indice) {
+        if (i !== j && caracter_a_comprobar === contenido_indice) {
+          comprobante = false;
+        }   
       }
     }
+
+    // Que no hayan más de 3 mayúsculas o minúscuals consecutivas.
+    let may_consecutivas = /[A-Z]{3}/;
+    let min_consecutivas = /[a-z]{3}/;
+
+    if (may_consecutivas.test(contrasenya) || min_consecutivas.test(contrasenya)) {
+      comprobante = false;
+    }
   }
-
-  // Que no hayan carácteres repetidos.
-
-
-
-
-}
+} while (comprobante = false);
